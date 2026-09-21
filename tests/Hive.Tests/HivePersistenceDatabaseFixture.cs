3de700d17bf5b2f5e440cbd5ad9fb7d4fe2091d8
Hani <tornadofay@gmail.com>
2026-09-21 08:13:07 +0300

    Hive: recreate persistence test database for a clean test run

--- tests/Hive.Tests/HivePersistenceDatabaseFixture.cs
+++ tests/Hive.Tests/HivePersistenceDatabaseFixture.cs
@@ -12,7 +12,7 @@ public sealed class HivePersistenceDatabaseFixture
     {
         Options = new HiveDatabaseOptions(HivePersistenceTestConfiguration.ConnectionString);
         DatabaseName = Options.DatabaseName;
-        EnsureDatabase.For.SqlDatabase(Options.ConnectionString);
+        RecreateDatabase();
         Console.WriteLine($"Hive persistence test database: {DatabaseName}");
     }
 
@@ -32,8 +32,39 @@ public sealed class HivePersistenceDatabaseFixture
 
             IF OBJECT_ID(N'dbo.HiveSchemaVersion', N'U') IS NOT NULL
                 DROP TABLE [dbo].[HiveSchemaVersion];
+
+            IF OBJECT_ID(N'dbo.HiveMigrationFailureProbe', N'U') IS NOT NULL
+                DROP TABLE [dbo].[HiveMigrationFailureProbe];
             """;
         command.ExecuteNonQuery();
+    }
+
+    private void RecreateDatabase()
+    {
+        var masterBuilder = new SqlConnectionStringBuilder(Options.ConnectionString)
+        {
+            InitialCatalog = "master"
+        };
+
+        using (var connection = new SqlConnection(masterBuilder.ConnectionString))
+        {
+            connection.Open();
+
+            using var command = connection.CreateCommand();
+            var escapedName = DatabaseName.Replace("]", "]]", StringComparison.Ordinal);
+            var quotedLiteral = DatabaseName.Replace("'", "''", StringComparison.Ordinal);
+
+            command.CommandText = $"""
+                IF DB_ID(N'{quotedLiteral}') IS NOT NULL
+                BEGIN
+                    ALTER DATABASE [{escapedName}] SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+                    DROP DATABASE [{escapedName}];
+                END;
+                """;
+            command.ExecuteNonQuery();
+        }
+
+        EnsureDatabase.For.SqlDatabase(Options.ConnectionString);
     }
 
     private SqlConnection CreateMasterOrDatabaseConnection()
