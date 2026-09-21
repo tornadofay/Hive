24f4a08e7452413de5f2f72e006e51d00cb299bf
Hani <tornadofay@gmail.com>
2026-09-21 08:33:50 +0300

    Hive: add HiveButton UI contract

---
+++ src/Hive.Host.WinForms.UI/Controls/HiveButton.cs
@@ -0,0 +1,54 @@
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using ReaLTaiizor.Controls;
+using Hive.Host.WinForms.UI.Theme;
+
+namespace Hive.Host.WinForms.UI.Controls;
+
+public sealed class HiveButton : UserControl
+{
+    private readonly MaterialButton _renderer;
+
+    public HiveButton()
+    {
+        _renderer = new MaterialButton
+        {
+            AutoSize = false,
+            Dock = DockStyle.Fill,
+            HighEmphasis = true,
+            Text = "Button",
+            UseAccentColor = true,
+            Depth = 1
+        };
+
+        _renderer.Click += (_, e) => OnClick(e);
+        Controls.Add(_renderer);
+
+        MinimumSize = new Size(88, 36);
+        Size = new Size(120, 40);
+        Margin = new Padding(0);
+    }
+
+    [Browsable(true)]
+    [DefaultValue("Button")]
+    public override string Text
+    {
+        get => _renderer.Text;
+        set => _renderer.Text = value;
+    }
+
+    [DefaultValue(true)]
+    public bool HighEmphasis
+    {
+        get => _renderer.HighEmphasis;
+        set => _renderer.HighEmphasis = value;
+    }
+
+    internal void ApplyTheme(HiveThemeDefinition theme)
+    {
+        _renderer.BackColor = theme.Palette.Accent;
+        _renderer.ForeColor = theme.Palette.AccentForeground;
+        _renderer.UseAccentColor = true;
+    }
+}
