9792e72d7c665c68e8ed944dcabcf9bbdafc2435
Hani <tornadofay@gmail.com>
2026-09-21 23:49:57 +0300

    revert: remove unrelated HivePage theme hook

--- src/Hive.Host.WinForms.UI/Theme/HiveThemeManager.cs
+++ src/Hive.Host.WinForms.UI/Theme/HiveThemeManager.cs
@@ -184,8 +184,8 @@ public sealed class HiveThemeManager : IHiveThemeManager
                 editorLayout.ApplyTheme(theme);
                 break;
 
-            case HivePage page:
-                page.ApplyTheme(theme);
+            case HiveListPageLayout listPageLayout:
+                listPageLayout.ApplyTheme(theme);
                 break;
 
             case HivePaginationBar paginationBar:
