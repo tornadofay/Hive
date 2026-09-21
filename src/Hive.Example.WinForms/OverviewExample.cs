2bb2113b6dec19c4468ef5a1f8ea11a1565de049
Hani <tornadofay@gmail.com>
2026-09-22 00:35:54 +0300

    Add Example Host project overview entry

---
+++ src/Hive.Example.WinForms/OverviewExample.cs
@@ -0,0 +1,20 @@
+using System.Windows.Forms;
+
+namespace Hive.Example.WinForms;
+
+internal sealed class OverviewExample : IHiveExample
+{
+    public string Category => "Overview";
+
+    public string Subcategory => "Getting Started";
+
+    public int Order => 0;
+
+    public string Title => "Project Overview";
+
+    public UserControl CreateView(IServiceProvider services)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+        return new OverviewExampleView();
+    }
+}
