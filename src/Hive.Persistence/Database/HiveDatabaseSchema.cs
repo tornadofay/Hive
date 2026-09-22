02dc490bd83314824d3d899a300fb10c3b6dffd5
Hani <tornadofay@gmail.com>
2026-09-22 10:31:44 +0300

    feat(persistence): advance schema for provider credential references

--- src/Hive.Persistence/Database/HiveDatabaseSchema.cs
+++ src/Hive.Persistence/Database/HiveDatabaseSchema.cs
@@ -2,7 +2,7 @@ namespace Hive.Persistence;
 
 public static class HiveDatabaseSchema
 {
-    public const int CurrentSchemaVersion = 7;
+    public const int CurrentSchemaVersion = 8;
     public const int MinimumSupportedSchemaVersion = 1;
 
     public const byte SchemaRowId = 1;
