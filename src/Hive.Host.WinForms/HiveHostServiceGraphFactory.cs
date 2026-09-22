f22aafe8d8262c4439cac576988015f0fbd3b80e
Hani <tornadofay@gmail.com>
2026-09-23 01:06:08 +0300

    fix(host): keep SQL initialization out of Example Host startup

--- src/Hive.Host.WinForms/HiveHostServiceGraphFactory.cs
+++ src/Hive.Host.WinForms/HiveHostServiceGraphFactory.cs
@@ -89,16 +89,6 @@ public sealed class SqlHiveHostServiceGraphFactory :
                         exception.Message));
             }
 
-            var migration = await new HiveDatabaseMigrator(options)
-                .MigrateAsync(cancellationToken)
-                .ConfigureAwait(false);
-
-            if (migration.IsFailure)
-            {
-                return Result<HiveHostServiceGraph>.Failure(
-                    migration.Error!);
-            }
-
             var management = new HiveManagementFacade(
                 new SqlProviderResourceStore(options),
                 new SqlAgentDefinitionResourceStore(options),
